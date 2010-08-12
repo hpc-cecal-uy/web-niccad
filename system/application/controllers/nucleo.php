7f7d9fe43e9ff7cf27102b42532983cdff56a381
santiago.iturriaga <santiago.iturriaga@8152ec25-3730-cbcc-c701-fa9584959b4c>
2010-08-12 13:00:09 +0000

    Se crea el sitio NICCAD

---
+++ system/application/controllers/nucleo.php
@@ -0,0 +1,22 @@
+<?php  
+
+class Nucleo extends Controller {
+	
+	function Nucleo() {
+		parent::Controller();
+	} 
+	
+	function index() {
+		$data['body'] = 'nucleo';
+		$data['volver_style'] = '';
+		$data['js_include'] = 
+			'<script src="'.base_url().'js/nucleo.js" type="text/javascript" charset="utf-8"></script>'.
+			'<script src="'.base_url().'js/jquery.featureList-1.0.0.js" type="text/javascript" charset="utf-8"></script>';
+		$data['css_include'] =
+			'<link rel="stylesheet" href="'.base_url().'css/nucleo.css" type="text/css" media="screen" />';
+		$this->load->view('includes/template', $data);
+	}
+
+}
+
+?>
